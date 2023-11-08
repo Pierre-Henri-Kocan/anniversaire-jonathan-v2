fb15662dc559d1f9e7fbd850423e2709e9797b60
ph.kocan <ph.kocan@gmail.com>
2023-11-08 10:54:08 +0100

    header and footer updated

--- php/inc/templates/header.tpl.php
+++ php/inc/templates/header.tpl.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="php/public/css/reset.css">
     <link rel="stylesheet" href="php/public/css/style.css">
     <link rel="shortcut icon" href="php/public/images/favicon.ico" type="image/x-icon">
